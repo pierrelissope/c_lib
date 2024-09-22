cd7f1ad413c5ad1b92a9dd213f82c7f77b76f410
Pierre <pierrelissope@gmail.com>
2024-09-22 15:42:17 +0200

    feat: added stdlib and string headers

---
+++ lib/include/my_stdlib.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#define CHUNK_METADATA_SIZE sizeof(chunk_metadata)
+
+typedef struct chunk_metadata_s {
+    size_t size;
+    struct chunk_metadata_s *next;
+} chunk_metadata;
+
+typedef struct {
+    void *stack_bottom;
+    chunk_metadata *used;
+} alloc_tracker;
+
+void *my_malloc(size_t size, alloc_tracker *tracker);
+void *my_realloc(void *ptr, size_t size, alloc_tracker *tracker);
+void my_free(void *ptr, alloc_tracker *tracker);
+void my_free_all(alloc_tracker *tracker);
+
+alloc_tracker new_tracker(void);
+void tracker_add(alloc_tracker *tracker, chunk_metadata *data);
+void tracker_remove(alloc_tracker *tracker, chunk_metadata *data);
---
+++ lib/include/my_string.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdlib.h>
+
+void *my_memset(void *dest, int c, size_t size);
+void *my_memcpy(void *dest, void *src, size_t size);
